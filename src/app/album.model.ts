ddb541ba024c757e3b91c9785f2b2757c8c177a2
rm <biffbangpow767@yahoo.com>
2017-11-03 10:58:54 -0700

    add dependency injections

---
+++ src/app/album.model.ts
@@ -0,0 +1,3 @@
+export class Album {
+  constructor (public artist: string, public title: string, public description: string, public id: number) { }
+}
--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Album } from '../album.model';
+import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
+
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
+
 })
-export class MarketplaceComponent implements OnInit {
 
-  constructor() { }
+export class MarketplaceComponent {
 
-  ngOnInit() {
-  }
+  constructor(private router: Router, private albumService: AlbumService){}
+
+  albums: Album[];
+
+goToDetailPage(clickedAlbum: Album) {
+     this.router.navigate(['albums', clickedAlbum.id]);
+   };
 
 }
